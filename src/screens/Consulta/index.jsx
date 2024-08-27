b3c2d4d87316c20e1c0d4abc26dbad745312818e
ArthurMorenoDev <arthurmoreno31@gmail.com>
2024-08-27 10:19:51 -0300

    Adição de telas na sidebar

---
+++ src/screens/Consulta/index.jsx
@@ -0,0 +1,10 @@
+const Consulta = () =>{
+    return(
+        <div className="content-area">
+            TELA DE consultas
+            <input type="text" ></input>
+        </div>
+    );
+};
+
+export default Consulta;
