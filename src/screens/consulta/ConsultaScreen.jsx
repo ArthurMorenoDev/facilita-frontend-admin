dd0233cce2e868ec0a69ca612a0b0a513805bbb5
ArthurMorenoDev <arthurmoreno31@gmail.com>
2024-08-28 15:25:48 -0300

    Layout da pagina de consulta

--- src/screens/consulta/ConsultaScreen.jsx
+++ src/screens/consulta/ConsultaScreen.jsx
@@ -1,8 +1,28 @@
-const Consulta = () =>{
-    return(
-        <div className="content-area">
-            TELA DE consultas
-            <input type="text" ></input>
+import "./styles.css"
+
+const Consulta = () => {
+    return (
+        <div>
+            <form>
+                <div className="consulta-container">
+                    <div className="consulta">
+                        <label className="label">CPF</label>
+                        <input type="text" className="input2"></input>
+                    </div>
+
+                    <div className="consulta">
+                        <label className="label">Matricula</label>
+                        <input type="number" className="input2"></input>
+                    </div>
+
+                    <div className="consulta">
+                        <label className="label">CPF</label>
+                        <input type="text" className="input2"></input>
+                    </div>
+                    <button type="submit" className="btn">Consultar</button>
+                </div>
+
+            </form>
         </div>
     );
 };
