951d8fcecd3f2ba3725995f8ac20568c9b3b5996
Arthur <arthurmoreno31@gmail.com>
2024-09-09 12:25:04 -0300

    Get Tabela Solicitações

--- src/screens/tabelas/TabelaScreen.jsx
+++ src/screens/tabelas/TabelaScreen.jsx
@@ -1,47 +1,40 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import AreaTable from "../../components/dashboard/areaTable/AreaTable";
-import Comission from '../../components/comission/Comission';
+import Comission from "../../components/comission/Comission";
 
 const Tables = () => {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true); // Adicione o estado de carregamento
+  const [solicitacoes, setSolicitacoes] = useState([]);
+  const [loading, setLoading] = useState(false); // Adicione o estado de carregamento
 
-  const getData = async () => {
+  const getSolicitacoes = async () => {
     try {
-      const token = localStorage.getItem('token'); // Obtenha o token do localStorage
-
-      // Simule um atraso de 3 segundos antes de fazer a requisição
-      setTimeout(async () => {
-        try {
+      setLoading(true);
+      const token = localStorage.getItem("token"); // Obtenha o token do localStorage
       // Faz a requisição GET com o token de autenticação
       const res = await axios.get("http://localhost:3000/listar-dados", {
         headers: {
           Authorization: `Bearer ${token}`, // Envia o token no cabeçalho Authorization
         },
       });
-
-      setData(res.data.data); // Define os dados retornados na resposta
-        } catch (error) {
-          console.error("Erro ao buscar os dados:", error);
-        } finally {
-          setLoading(false); // Atualiza o estado de carregamento para false
-        }
-      }, 3000); // Atraso de 3 segundos
+      setLoading(false);
+      setSolicitacoes(res.data.data); // Define os dados retornados na resposta
+      console.log(res.data);
     } catch (error) {
+      setLoading(false);
       console.error("Erro ao buscar os dados:", error);
-      setLoading(false); // Atualiza o estado de carregamento mesmo se ocorrer um erro
     }
   };
 
   useEffect(() => {
-    getData();
+    getSolicitacoes();
   }, []); // O segundo argumento [] garante que o useEffect execute apenas uma vez, quando o componente for montado
 
   return (
     <div className="content-area">
       <Comission />
-      <AreaTable data={data} loading={loading} /> {/* Passe a prop de carregamento */}
+      <AreaTable data={solicitacoes} loading={loading} />{" "}
+      {/* Passe a prop de carregamento */}
     </div>
   );
 };
